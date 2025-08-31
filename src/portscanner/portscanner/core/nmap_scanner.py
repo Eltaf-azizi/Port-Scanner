bbde16d28df988731172c8359c8173876d7efc15
Eltaf-azizi <iltfhuss@gmail.com>
2025-08-31 22:21:55 +0500

    working on the python nmap interprets arguments as string

--- src/portscanner/portscanner/core/nmap_scanner.py
+++ src/portscanner/portscanner/core/nmap_scanner.py
@@ -16,3 +16,25 @@ class NmapScanner(ScannerBase):
         results: Dict[str, Any] = {}
         target_str = " ".join(targets)
         port_str = ",".join(str(p) for p in ports)
+
+        # python-nmap interprets arguments as string. Timeout not directly applied; user can adapt arguments.
+        self.nm.scan(hosts=target_str, ports=port_str, arguments=self.arguments)
+        for host in self.nm.all_hosts():
+            results.setdefault(host, {})
+            tcpdata = self.nm[host].get('tcp', {}) or {}
+            udpdata = self.nm[host].get('udp', {}) or {}
+
+            # combine
+            for p, info in {**tcpdata, **udpdata}.items():
+                results[host][int(p)] = {
+                    "state": info.get('state', 'unknown'),
+                    "meta": {
+                        "name": info.get('name'),
+                        "product": info.get('product'),
+                        "version": info.get('version'),
+                        "extrainfo": info.get('extrainfo'),
+                        "protocol": 'tcp' if int(p) in tcpdata else 'udp'
+                    }
+                }
+                
+        return results
